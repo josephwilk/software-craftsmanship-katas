8e23af37e1683a227b6a38af81176d8dafac787e
Joseph Wilk <joe@josephwilk.net>
2009-08-08 14:10:21 +0100

    typo

--- ruby/bowling/lib/bowling/game.rb
+++ ruby/bowling/lib/bowling/game.rb
@@ -10,30 +10,30 @@ module Bowling
     end
 
     def score
-      @frames.reverse.reduce(0) {|score_total, frame| score_total += frame.score}
+      @frames.reduce(0) {|score_total, frame| score_total += frame.score}
     end
 
     private
     def current_frame
-      frame = last_game
+      frame = last_frame
       if frame.nil? || frame.complete?
         @frames << Frame.new
-        frame.next_frame = last_game unless frame.nil?
-        frame = last_game
+        frame.next_frame = last_frame unless frame.nil?
+        frame = last_frame
       end
       frame
     end
 
     def game_ended?
-      (@frames.size == 10 && last_game.complete? && !special_last_game?) ||
+      (@frames.size == 10 && last_frame.complete? && !special_last_game?) ||
       (@frames.size == 12 )
     end
 
     def special_last_game?
-      last_game.strike? || last_game.spair? 
+      last_frame.strike? || last_frame.spair? 
     end
 
-    def last_game
+    def last_frame
       @frames.last
     end
   end
